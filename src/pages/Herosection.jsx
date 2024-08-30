a037a853eece35d6d7342c0c23feddfb6e205489
Vinay Thakor <vinay.th.24@gmail.com>
2024-08-30 22:37:46 +0530

    first day

---
+++ src/pages/Herosection.jsx
@@ -0,0 +1,13 @@
+import Landing from '../components/Landing';
+import Features from '../components/Features';
+
+const Herosection = () => {
+  return (
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[1517px] box-border gap-[116px] leading-[normal] tracking-[normal] gap-[29px] gap-[58px]">
+      <Landing />
+      <Features />
+    </div>
+  );
+};
+
+export default Herosection;
