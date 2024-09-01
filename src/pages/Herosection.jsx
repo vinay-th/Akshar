ca703ca30acba3af640de9be0d8cb985ff3f5d6c
Vinay Thakor <vinay.th.24@gmail.com>
2024-09-01 14:13:39 +0530

    testimonial-final

--- src/pages/Herosection.jsx
+++ src/pages/Herosection.jsx
@@ -5,7 +5,7 @@ import Testimonials from '../components/Testimonials';
 
 const Herosection = () => {
   return (
-    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[1517px] box-border leading-[normal] tracking-[normal]">
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[1000px] box-border leading-[normal] tracking-[normal]">
       <Landing />
       <Features />
       <Testimonials />
