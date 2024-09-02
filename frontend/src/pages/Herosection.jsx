c3a044164e6ff89c7826b9de120f4dd2a40ed0a5
Vinay Thakor <vinay.th.24@gmail.com>
2024-09-02 16:50:01 +0530

    file restructure all files paths were changed and App and API works

---
+++ frontend/src/pages/Herosection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Landing from '../components/Landing';
+import Features from '../components/Features';
+import Testimonials from '../components/Testimonials';
+
+const Herosection = () => {
+  return (
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[1000px] box-border leading-[normal] tracking-[normal]">
+      <Landing />
+      <Features />
+      <Testimonials />
+    </div>
+  );
+};
+
+export default Herosection;
